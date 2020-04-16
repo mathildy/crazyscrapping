76da14ee9bcc2c103b6d69de3d3e4579cd38cee6
mathildy <mathilde.prouchandy@gmail.com>
2020-04-16 22:13:59 +0200

    UPDATE

--- lib/mairies.rb
+++ lib/mairies.rb
@@ -1,12 +1,27 @@
 require 'nokogiri'
 require 'open-uri'
 
-def get_townhall_email(townhall_url)
-    page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
-    page.css('a.lientxt').each do |node|
+def emails
+    
+    doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    doc.css('a.lientxt').each do |node|
+    @liens=[(node['href'].slice!(1,1000))]  
+            
+        @villes = node.text
+        def list_emails
+
+            doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com#{@liens[0]}"))
+            doc.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |node|
+        
+            mails = node.text
+            puts "#{@villes} => #{mails}."
+            end
+    
+        end
+
+    list_emails
     end
 
+end
 
-
-
-end 
+emails
